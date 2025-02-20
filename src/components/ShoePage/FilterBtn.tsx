235aa1fd7b1075fbc1c273fc3e68e68edcc80a2f
abdoumes <154231201+Abdou373@users.noreply.github.com>
2025-02-20 15:04:58 +0100

    improve

--- src/components/ShoePage/FilterBtn.tsx
+++ src/components/ShoePage/FilterBtn.tsx
@@ -1,16 +1,28 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { FaFilter } from "react-icons/fa";
 
 export default function FilterBtn() {
   const [open, setOpen] = useState(false);
+  const input = useRef<HTMLInputElement>(null);
   return (
     <div className="flex items-center gap-2 relative">
-      <div onClick={() => { open ? setOpen(false) : setOpen(true) }} className='h-10 w-10 bg-black cursor-pointer'></div>
+      <div onClick={() => { open ? setOpen(false) : setOpen(true) }} className='cursor-pointer'><FaFilter className="size-6 text-gray-500" /></div>
       {open && (
         <form className="flex gap-2">
-          <input type="number" className="bg-[#eee] px-4 py-2 rounded-10" placeholder="Expensive than :" />
-          <input type="number" className="bg-[#eee] px-4 py-2 rounded-10" placeholder="Cheaper than :" />
+          {/* <div className="relative">
+            <input name="exp" type="number" className="bg-[#eee] px-4 py-2 w-full rounded-10" />
+            <label className="absolute z-10 left-1/2 -translate-x-1/2 text-lg text-nowrap top-1/2 -translate-y-1/2" htmlFor="exp">Expensive Than</label>
+          </div> */}
+          {/* <div className="relative">
+            <input name="cheaper" type="number" className="bg-[#eee] px-4 py-2 w-full rounded-10" />
+            <label className="absolute z-10 left-1/2 -translate-x-1/2 text-lg text-nowrap top-1/2 -translate-y-1/2" htmlFor="cheaper">Cheaper than</label>
+          </div> */}
+          <div onClick={() => { input.current?.classList.remove("hidden"); input.current?.classList.add("input"); input.current?.focus() }} className="bg-white px-4  rounded-md shadow-md flex items-center gap-2 cursor-pointer">
+            <label className="px-1 text-black">Expensive Than</label>
+            <input className="text-black px-1 w-20 outline-none hidden" ref={input} type="number" max={5} required />
+          </div>
           <div className="bg-[#eee] px-4 py-2 rounded-10">Rating</div>
         </form>
       )}
